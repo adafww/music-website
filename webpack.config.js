f64855a3ba6871d857a1588e8ad7f8e6314e8385
adafww <mxhx23@gmail.com>
2025-03-30 21:10:48 +0300

    first

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html', // Убедиcь, что путь корректный
+      filename: 'index.html'
+    })
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+  }
+};
